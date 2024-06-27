0814aa8bd9b64bc9cb81347f27c47586d6a1ef75
sareh-srm <sareh.82.srm@gmail.com>
2024-06-27 15:58:27 +0330

    fix: Fix form data handling in signup.php

--- db/signup.php
+++ db/signup.php
@@ -5,11 +5,15 @@ $password = "";
 $dbname = "mands";
 
 $sql1 = mysqli_connect("$servername", "$username", "$password", "$dbname");
+$sql2 = "INSERT INTO signup (name, l_name, u_name, p_word, fav) VALUES ('$_POST[name]', '$_POST[l_name]', '$_POST[u_name]', '$_POST[p_word]', '$_POST[fav]')";
 
-if($sql1)
-echo "connected";
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+if(mysqli_query($sql1, $sql2))
+  echo "<h1>Welcome!</h1>";
 
 else
-echo "error";
-
+  echo "error";
 ?>
